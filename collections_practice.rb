85ced0758d4ebdb109b0cb79cdfa79cb05257d27
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-13 20:42:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,32 +2,77 @@
 def begins_with_r(array)
   out = true
   array.each do |x|
-    out = out && x.[0]=="r"
+    out = out && x[0]=="r"
   end
   out
 end
 
 def contain_a(array)
-  array.collect do |x|
+  out = []
+  array.each do |x|
     if x.include?("a")
-      x
+      out << x
     end
   end
+  out
 end
 
 def first_wa(array)
   array.each do |x|
-    if x.include?("wa")
-      return x
+    if x.instance_of?(String)
+      if x.include?("wa")
+        return x
+      end
     end
   end
 end
 
 def remove_non_strings(array)
-  array.collect do |x|
-   if foo.instance_of?(String)
-     x
+  out = []
+  array.each do |x|
+   if x.instance_of?(String)
+     out << x
    end
- end
+  end
+  out
 end
- 
+
+def count_elements(array)
+  temp = array
+  out = []
+  index = 0
+  i = 0
+  while i < temp.length
+    out << {}
+    out[index][:count] = temp.find(element).length
+    temp = temp.delete(element)
+    i += 1
+  end
+end
+
+def merge_data(keys, data)
+  out = []
+  puts data
+  i=0
+  
+  while i < keys.length
+    out << {}
+    
+    keys[i].each do |title, name|
+      out[i][title] = name
+    end
+    
+    data[i].each do |datum, about|
+      about.each do |stat, val|
+        out[i][stat] = val
+      end
+    end
+    i += 1
+  end
+end
+
+def find_cool(cool)
+end
+
+def organize_schools(schools)
+end
