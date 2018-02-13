3ca8d9df381afac479aa9d9ea550e4d57e6ba316
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-13 21:02:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,8 +41,7 @@ def count_elements(array)
   temp = array
   out = []
   index = 0
-  fuckyou = temp.size
-  while fuckyou > 0 do
+  while temp.size > 0 do
     out << {}
     found = temp.find_all {|i| i == temp[0] }
     out[index][:count] = found.size
