45bb7dc90d841ed659215931fc5100adf754c6d7
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-13 21:12:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -90,3 +90,18 @@ keys =     [
             :first_name => "ashley"
         }
     ]
+data =     [
+           {
+             "blake" => {
+                :awesomeness => 10,
+                     :height => "74",
+                  :last_name => "johnson"
+            },
+            "ashley" => {
+                :awesomeness => 9,
+                     :height => 60,
+                  :last_name => "dubs"
+            }
+        }
+    ]
+merge_data(keys,data)
